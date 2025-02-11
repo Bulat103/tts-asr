c0b2863a9dfe8882098a9222a750016e66fe495f
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2025-02-11 14:55:49 +0300

    added tts model preload, changed vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import * as tts from '@diffusionstudio/vits-web';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [text, setText] = useState('');
   const [voiceloading, setVoiceLoading] = useState(false);
+
+  useEffect(() => {
+    void tts.download('ru_RU-dmitri-medium', (progress) => {
+      console.log(
+        `Downloading ${progress.url} - ${Math.round(
+          (progress.loaded * 100) / progress.total,
+        )}%`,
+      );
+    });
+  }, []);
 
   const onClick = async () => {
     if (!text) return;
